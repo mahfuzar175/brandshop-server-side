cc49a976af15d2898adabe3567af491a06f7b796
mahfuzar175 <mahfuzar175@gmail.com>
2023-10-19 14:47:51 +0600

    create a post for show a single product from the database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,6 +32,13 @@ async function run() {
     app.get('/products', async(req, res) =>{
       const cursor = productCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/products/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await productCollection.findOne(query);
       res.send(result);
     })
 
